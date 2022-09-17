48a756fa80189be4c66a72857eb21ff4864c42e0
TizianaParisi <tiziana01.parisi@edu.unife.it>
2022-09-17 17:36:52 +0200

    Aggiunto test per Profilo Utente

--- UniEasy/src/test/java/com/mygroup/app/UniEasy/ProfiloUtenteTest.java
+++ UniEasy/src/test/java/com/mygroup/app/UniEasy/ProfiloUtenteTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class ProfiloUtenteTest {
-
 	
 	// Test che verifica se vengono restituite le informazioni per il docente in base all'username fornito
 	@Test
@@ -29,8 +28,7 @@ class ProfiloUtenteTest {
 		assertNull(profilo.ottieniProfiloDocente(username));
 		
 	}
-	
-	
+		
 	// Test che verifica se vengono restituite le informazioni per lo studente in base alla matricola fornita
 	@Test
 	void whenStudenteEsiste_expectInfoStudente() {
@@ -42,8 +40,7 @@ class ProfiloUtenteTest {
 		assertNotNull(profilo.ottieniProfiloStudente(username));
 			
 	}
-	
-	
+		
 	// Test che verifica il caso in cui non vengono fornite informazioni data una matricola non presente nel DB
 	@Test
 	void whenStudenteNonEsiste_expectNull() {
